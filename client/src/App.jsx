60ed6f65a20553962156b7debbeaa7b59ba77418
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-09-20 12:01:10 -0400

    [STYLE] Basic commenting throughout application component file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,19 @@ import ContactForm from "./ContactForm";
 import ResponseModal from "./ResponseModal";
 import "./App.css";
 
+// Main application logic/scripting
 function App() {
-  const [submitted, setSubmitted] = useState(false);
-  const [spamDetected, setSpamDetected] = useState(false);
+	// Create getter/setter for form submission and spam detection flags
+  	const [submitted, setSubmitted] = useState(false);
+  	const [spamDetected, setSpamDetected] = useState(false);
 
-  return (
-    <div className="App">
-      <ContactForm setSubmitted={setSubmitted} setSpamDetected={setSpamDetected}/>
-      {submitted ? <ResponseModal spamDetected={spamDetected} /> : null}
-    </div>
-  );
+	// Serve overall JSX layout for web app using contact form and response modal
+  	return (
+    	<div className="App">
+      		<ContactForm setSubmitted={setSubmitted} setSpamDetected={setSpamDetected}/>
+      		{submitted ? <ResponseModal spamDetected={spamDetected} /> : null}
+    	</div>
+  	);
 }
 
 export default App;
