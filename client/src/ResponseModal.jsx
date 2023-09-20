00d22307eafb2bb6f12ce6e2398fbf3f67f911f2
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-09-20 12:01:43 -0400

    [STYLE] Basic commenting throughout response modal component file

--- client/src/ResponseModal.jsx
+++ client/src/ResponseModal.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 
+// Creation of response modal component with spam detection flag tracked as prop
 const ResponseModal = ({ spamDetected }) => {
+	// Conditionally render email-block text if spam was detected from model
     if (spamDetected) {
         return (
-          <StyledResponseModal>
-            <h2>Uh-oh!</h2>
-            <div>Your message may or may not contain spam content and has been auto-blocked. Please re-submit or try again later!</div>
-          </StyledResponseModal>
+        	<StyledResponseModal>
+            	<h2>Uh-oh!</h2>
+            	<div>Your message may or may not contain spam content and has been auto-blocked. Please re-submit or try again later!</div>
+          	</StyledResponseModal>
         )
-      } else if (!spamDetected) {
+    } 
+	// Conditionally render email-sent text if spam was not detected from model
+	else if (!spamDetected) {
         return (
-          <StyledResponseModal>
-            <h2>Thanks!</h2>
-            <div>We'll be in touch soon.</div>
-          </StyledResponseModal>
+        	<StyledResponseModal>
+            	<h2>Thanks!</h2>
+            	<div>We'll be in touch soon.</div>
+          	</StyledResponseModal>
         )
-      }
+    }
 
-    return (
-    <div></div>
-    );
+	// Generic empty JSX return 
+    return (<div></div>);
 };
 
 export default ResponseModal;
 
-// Styles
+// Response modal component styling
 const StyledResponseModal = styled.div`
 position: fixed;
 top: 50%;
